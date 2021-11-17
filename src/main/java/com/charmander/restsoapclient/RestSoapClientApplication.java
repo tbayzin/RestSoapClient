8cb2999a67621218de0fc28704bc225cb5f109c9
tarikb <bayzin@hotmail.com.tr>
2021-11-17 17:14:44 +0300

    First commit DW

---
+++ src/main/java/com/charmander/restsoapclient/RestSoapClientApplication.java
@@ -0,0 +1,13 @@
+package com.charmander.restsoapclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSoapClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestSoapClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/charmander/restsoapclient/RestSoapClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charmander.restsoapclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSoapClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
