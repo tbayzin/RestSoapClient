02332ebbd31acd0298901d4e5ee1f206f46840ac
tarikb <bayzin@hotmail.com.tr>
2021-11-18 09:34:25 +0300

    z

---
+++ src/main/java/com/charmander/restsoapclient/SoapClient.java
@@ -0,0 +1,15 @@
+package com.charmander.restsoapclient;
+
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+
+import javax.xml.bind.JAXBElement;
+
+public class SoapClient {
+
+    public class SoapClient extends WebServiceGatewaySupport {
+        public GetStateResponseType getBank(String url, Object request) {
+            JAXBElement res = (JAXBElement) getWebServiceTemplate().marshalSendAndReceive(url, request);
+            return (GetStateResponseType) res.getValue();
+        }
+    }
+}
